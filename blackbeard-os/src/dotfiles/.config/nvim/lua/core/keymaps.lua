184df7a741ede200c7df71f4893a8d7fe091d496
cvusmo <blacksheepcosmo@gmail.com>
2024-04-21 03:44:18 -0500

    added script for wofi to auto generate style.css and a waybar script to autogenerate style.css using color.conf

--- blackbeard-os/src/dotfiles/.config/nvim/lua/core/keymaps.lua
+++ blackbeard-os/src/dotfiles/.config/nvim/lua/core/keymaps.lua
@@ -8,3 +8,4 @@ map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = t
 -- clear search with esc
 map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and Clear hlsearch" })
 
+
