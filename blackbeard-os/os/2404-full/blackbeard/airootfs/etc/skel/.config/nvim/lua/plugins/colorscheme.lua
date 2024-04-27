b885f8420304f1d27ec42eaba9e615f7ba16edd7
cvusmo <blacksheepcosmo@gmail.com>
2024-04-27 12:09:25 -0500

    updated skel/.config

--- blackbeard-os/os/2404-full/blackbeard/airootfs/etc/skel/.config/nvim/lua/plugins/colorscheme.lua
+++ blackbeard-os/os/2404-full/blackbeard/airootfs/etc/skel/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,22 +1,21 @@
---Set Transparency
 local is_transparent = false
 if vim.fn.has("unix") == 1 then
   is_transparent = true
 end
 
-return
-{
-    "scottmckendry/cyberdream.nvim",
-    lazy = false,
-    priority = 998,
-    config = function()
-      require("cyberdream").setup({
-            transparent = is_transparent,
-            italic_comments = true,
-            hide_fillchars = true,
-            --borderless_telescope = false,
-            --terminal_colors = true,
-      })
-      vim.cmd("colorscheme cyberdream")
+return {
+  "neanias/everforest-nvim",
+  lazy = false,
+  priority = 998,
+  config = function() 
+    require("everforest").setup({
+      transparent = is_transparent,
+      italic_comments = true,
+      terminal_colors = true,
+      borderless_telescope = false,
+      background = "hard",
+      italics = true,
+    })
+    vim.cmd("colorscheme everforest")
   end,
 }
